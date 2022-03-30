5b738fb056b47073e9921da5f4283b79bd546aea
PZ <padhiez@hotmail.de>
2022-03-30 20:50:14 +0200

    add first step

---
+++ src/class/Stream.js
@@ -0,0 +1,13 @@
+class Stream {
+    name;
+    title;
+    image;
+    viewerCount;
+
+    constructor() {
+        this.name = '';
+        this.title = '';
+        this.image = '';
+        this.viewerCount = 0;
+    }
+}
